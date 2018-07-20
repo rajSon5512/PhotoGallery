2ec48993fe0492dbfb1592fe3048d167c964aba2
rajSon5512 <rjvsonawala@gmail.com>
2018-07-20 00:06:16 -0700

    Chapter 25

---
+++ app/src/main/java/com/knoxpo/rajivsonawala/photogalleryupdate/MainActivity.java
@@ -0,0 +1,13 @@
+package com.knoxpo.rajivsonawala.photogalleryupdate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
