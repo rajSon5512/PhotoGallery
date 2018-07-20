93759b6aa9836d0aab1c44bc76c39507c20a36b7
rajSon5512 <rjvsonawala@gmail.com>
2018-07-20 01:15:11 -0700

    Chapter 25

--- app/src/main/java/com/knoxpo/rajivsonawala/photogalleryupdate/MainActivity.java
+++ app/src/main/java/com/knoxpo/rajivsonawala/photogalleryupdate/MainActivity.java
@@ -1,13 +1,13 @@
 package com.knoxpo.rajivsonawala.photogalleryupdate;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SingleFragmentActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        protected Fragment createFragment() {
+            return PhotoGalleryFragment.newInstance();
+        }
+
 }
